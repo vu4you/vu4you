7ca6c82f1d0c66ee9836978fce75923ea6d8585e
putte91 <patrik_910831@hotmail.com>
2014-04-01 16:40:18 +0200

    fuck this / rollback

---
+++ app/views/startpage.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Vi Unga 4 You - Startsida</title>
+	<style>
+		@import url(//fonts.googleapis.com/css?family=Lato:700);
+
+		body {
+			margin:0;
+			font-family:'Lato', sans-serif;
+			text-align:center;
+			color: #999;
+		}
+
+		.welcome {
+			width: 800px;
+			margin: auto;
+			margin-top: 10%;
+			-moz-box-shadow: 0 0 30px 2px black, 40px -30px lime, 40px 30px 50px red, -40px 30px yellow, -40px -30px 50px blue;
+			-webkit-box-shadow: 0 0 30px 2px black, 40px -30px lime, 40px 30px 50px red, -40px 30px yellow, -40px -30px 50px blue;
+			box-shadow: 0 0 30px 2px black, 150px 80px 130px lime, 130px -80px 50px red, -130px -80px 130px yellow, -150px 80px 50px blue;
+		}
+
+		a, a:visited {
+			text-decoration:none;
+		}
+
+		h1 {
+			font-size: 52px;
+			margin: 16px 0 0 0;
+		}
+		h2 {
+			font-size: 28px;
+			margin: 16px 0 0 0;
+		}
+	</style>
+</head>
+<body>
+	<div class="welcome">
+		<h1>Vi Unga 4 You</h1>
+			<br/>
+			<h2>Hemsidan är under konstruktion.</h2>
+		
+	</div>
+</body>
+</html>
