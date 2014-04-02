a3dbf78c1559529bf6008955ed1dcfa1791f8930
knugarn <stereotobbe@gmail.com>
2014-04-02 23:32:17 +0200

    added contact badges

--- app/controllers/ContactController.php
+++ app/controllers/ContactController.php
@@ -5,7 +5,13 @@ class ContactController extends BaseController {
 	
 	public function index()
 	{
-		return View::make('contact/index');
+		$contacts = array();
+		array_push($contacts, array("name"=>"Andreas Person Stenberg","role"=>"Ordförande","phone"=>"072-005 05 80", "email"=> "ordforande@vu4you.se"));
+		array_push($contacts, array("name"=>"Tobias Eklund","role"=>"Vice Ordförande","phone"=>"073-032 08 11","email"=> "viceordforande@vu4you.se"));
+		array_push($contacts, array("name"=>"Joel Eklund","role"=>"Kassör","email"=> "kassor@vu4you.se"));
+		array_push($contacts, array("name"=>"Patrik Andersson","role"=>"Ledarmot"));
+		array_push($contacts, array("name"=>"Emil Vesterberg","role"=>"Ledarmot"));
+		return View::make('contact/index')->with("contacts",$contacts);
 	}
 
 }
--- app/views/contact/index.blade.php
+++ app/views/contact/index.blade.php
@@ -3,5 +3,35 @@
 
 
 @section('content')
-    <p>Såhär kan du kontakta oss</p>
+    <div class="page-header">
+  		<h1>Kontakta <small>kontaktuppgifter till styrelsen</small></h1>
+	</div>
+	<div class="row">
+		@if (isset($contacts))
+			<?php $count = 0 ?>
+			@foreach ($contacts as $contact)
+
+  				<?php $count++ ?>
+    			<div class="col-sm-6 col-md-4 contact-thumb">
+      				<img class="col-md-12" src="http://placekitten.com/g/300/200" alt="..." />
+      				<div class="col-md-12">
+        				<h3>{{{$contact['name']}}} <small style="white-space:nowrap;">{{{$contact['role']}}}</small></h3>
+        				<address>
+        				@if (isset($contact['phone']))
+        					<strong>Telefon:</strong> {{{$contact['phone']}}} <br>
+						@endif
+						@if (isset($contact['email']))
+        					<strong>Email:</strong> <a href="mailto:{{{$contact['email']}}}">{{{$contact['email']}}}</a> <br>
+						@endif
+						</address>
+  					</div>
+  				</div>
+  				@if ($count%3==0)
+  					<div class="clearfix visible-md visible-lg"></div>
+  				@elseif ($count%2==0)
+  					<div class="clearfix visible-xs visible-sm"></div>
+  				@endif
+			@endforeach
+		@endif
+	</div>
 @stop
