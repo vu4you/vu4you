2ee9b7d9e44ac9203da78fd80c02f13b5ea7d94a
knugarn <stereotobbe@gmail.com>
2014-04-03 00:24:23 +0200

    Added analytics tracking

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -26,5 +26,15 @@
 
 	<script src="{{ asset('js/jquery-1.11.0.js')}}"></script>
 	<script src="{{ asset('js/bootstrap.js')}}"></script>
+    <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-40027679-1', 'vu4you.se');
+  ga('send', 'pageview');
+
+</script>
     </body>
 </html>
