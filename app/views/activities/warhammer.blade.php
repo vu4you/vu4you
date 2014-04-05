015c5743209893a4160851cf143c52e0668653bd
knugarn <stereotobbe@gmail.com>
2014-04-06 01:48:22 +0200

    Added some more elements to lan and warhammer activities

--- app/views/activities/warhammer.blade.php
+++ app/views/activities/warhammer.blade.php
@@ -3,5 +3,13 @@
 
 
 @section('content')
-    <p>Yey leka!.</p>
+  		
+	
+	<div class="page-header-img" style="background-image:url({{asset('img/warhammer.jpg')}});"></div>
+	<h1>Warhammer <small>verksamhet</small></h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean quam orci, dictum vitae justo sed, bibendum dignissim urna. Aenean ultricies arcu ipsum, eu bibendum turpis fringilla eu. Donec sagittis tortor ac lacus ultrices convallis. Integer consequat felis sed tellus facilisis, ac luctus massa ornare. Donec at nulla vestibulum elit volutpat vestibulum quis nec massa. Praesent venenatis tempus nibh, id convallis nibh commodo sit amet. Ut a risus eu risus dapibus suscipit nec id magna.</p>
+<p>Donec eros metus, tincidunt sit amet gravida sed, dapibus a eros. Vestibulum gravida commodo dui, non fermentum arcu lobortis quis. Nunc ut odio semper, elementum dui et, lacinia tellus. Aenean faucibus faucibus nisi, convallis volutpat ante ultricies sit amet. Phasellus in nisi porta, consectetur tortor ut, sagittis arcu. Integer porta eleifend tristique. Maecenas aliquam elit vel nunc lacinia, sed viverra purus convallis. Nunc tristique feugiat nisi id blandit. Aenean hendrerit egestas nunc eu interdum. Vivamus vitae sem lacus. Integer ut mollis massa, eget porttitor massa. Nunc vel lorem at erat dapibus viverra.</p>
+<p>Nunc id est elit. Sed blandit enim purus, quis commodo enim rutrum in. Aliquam feugiat facilisis cursus. In nec congue sem. Duis lobortis sem felis, et gravida nibh porttitor condimentum. Phasellus non commodo enim, eget laoreet urna. Nulla porta condimentum metus, eu sagittis ipsum iaculis et. Mauris sollicitudin lectus a lectus auctor adipiscing. Mauris mollis nisl convallis orci placerat, sit amet posuere dolor semper. Suspendisse potenti. Praesent pellentesque, lorem sed dapibus eleifend, neque metus vehicula sapien, vel molestie nisi elit non magna. Suspendisse ornare lacinia erat id facilisis.</p>
+<p>Maecenas vitae adipiscing nibh, et porttitor felis. Proin sagittis erat non lobortis ultricies. Duis sagittis orci eget ipsum tincidunt, id ultricies turpis dignissim. Suspendisse risus turpis, adipiscing eget turpis vitae, auctor tincidunt orci. In sem arcu, aliquet vel congue eget, pulvinar eu dolor. Proin suscipit eleifend quam et egestas. Pellentesque venenatis, tortor quis pellentesque placerat, nibh neque varius velit, vel semper lectus sem a lacus.</p>
+<p>Donec eu tempus orci, ut dapibus velit. Ut ultricies eros nec eros porta, id hendrerit diam suscipit. Vivamus luctus venenatis ornare. Maecenas id arcu nibh. Proin eu dictum ipsum. Curabitur ut pharetra felis. Morbi facilisis a risus nec tristique.</p>
 @stop
